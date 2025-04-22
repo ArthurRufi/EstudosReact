06a33f2a41afc090c5d4c976e0fd5d79744161b3
ArthurRufi <temparthur87@gmail.com>
2025-04-22 12:25:32 -0300

    metade do projeto pratico

---
+++ src/AddTasks.jsx
@@ -0,0 +1,5 @@
+function AddTasks(){
+    <h1 className="text-blue-600">Add Task</h1>
+}
+
+export default AddTasks
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import { useState } from "react"
+import AddTasks from "./AddTasks";
+import Tasks from "./components/Tasks";
+import './App.css';
+import { useState } from "react";
 
-function App(){
+function App() {
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    title: "Comprar Ração",
+    description: "Comprar 10kg de ração para o gato",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: "Reunião Pessoal",
+    description: "Reunião Pessoal com Arquiteta",
+    isCompleted: false,    
+  },
+  {
+    id: 3,
+    title: "Iniciar Projeto",
+    description: "Iniciar projeto do SaaS cliente 3031",
+    isCompleted: false,
+  }
   
-  const [message, setMessage] = useState("Pomba")
-
-  return (
-    <div>
-      <h1>{message}</h1>
-      <button 
-        onClick={() =>{
-          setMessage("Olá Cliquei Aqui")
-        }}
-      >
-        Mudar Mesagem
-      </button>
+  ])
+  return (  // Adicionando o return aqui para que o JSX seja retornado corretamente
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <ul className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </ul>
+        <Tasks tasks={tasks}/>
+        <AddTasks />
+      </div>
     </div>
-  
-  
-  
-  )
+  );
 }
-export default App
+
+export default App;
