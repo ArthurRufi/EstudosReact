8375de4ae814d8428cbf92bcec2846f7045971d1
ArthurRufi <temparthur87@gmail.com>
2025-04-22 16:18:25 -0300

    commit de seguranca

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,17 @@
 function Tasks(props){
     console.log(props)
     return (
-        <ul className="space-y-4">
-            {props.tasks.map((task) => 
-                <li className="bg-slate-400 text-white p-2 rounded-md ">{task.title}</li>
-            )}
+        <ul className="space-y-4 p-6 bg-slate-400 rounded-md shadow">
+            {props.tasks.map((task) => (
+                <li key="task.id" className="flex" >
+                    <button className="bg-slate-400 w-full text-white p-2 rounded-md ">
+                        {task.title}
+                    </button>
+                    <button className="bg-slate-400 text-white p-2 rounded-md ">
+                        Details
+                    </button>
+                </li>
+            ))}
         </ul>
     );
 }
